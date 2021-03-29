a9d755adf2d429844a3d85923e3d3267d360d7df
Paranthaman Pranavan <31650351+pranaone@users.noreply.github.com>
2021-03-29 09:31:54 +0530

    Add project files.

---
+++ Migrations/20210328054825_fix card number.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace PaymentAPI.Migrations
+{
+    public partial class fixcardnumber : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "CardNumber",
+                table: "PaymentDetails",
+                type: "nvarchar(16)",
+                maxLength: 16,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(100)",
+                oldMaxLength: 100,
+                oldNullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "CardNumber",
+                table: "PaymentDetails",
+                type: "nvarchar(100)",
+                maxLength: 100,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(16)",
+                oldMaxLength: 16,
+                oldNullable: true);
+        }
+    }
+}
